3f3c92b16b25eaace116820d9665713b55d22bfa
proudman36 <proudman36@hotmail.com>
2023-03-23 15:07:52 -0500

    Deletion of the forms route

--- Primavera/Primavera/urls.py
+++ Primavera/Primavera/urls.py
@@ -26,7 +26,6 @@ urlpatterns = [
     path('',include('home.urls')),
     path('',include('Accounts.urls')),
     path('',include('django.contrib.auth.urls')),
-    path('',include('forms.urls')),
     path('about/',include('about.urls')),
     path('blog/', include('blog.urls')),
     path('projects_primavera/', include('projects_primavera.urls')),
