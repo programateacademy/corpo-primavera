227a466fa5bd17169c19a26ebcb6efb1ba219202
proudman36 <proudman36@hotmail.com>
2023-03-23 10:18:56 -0500

    Addition of the form in the donations section

--- Primavera/donaciones/views.py
+++ Primavera/donaciones/views.py
@@ -1,5 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.urls import reverse
+from django.shortcuts import redirect
+from .forms import Contact_Form
 # Create your views here.
 def donaciones(response):
-    return render(response,'donaciones.html',{})
+
+    contact_form = Contact_Form()
+    if response.method == 'POST':
+        contact_form = Contact_Form(data=response.POST)
+
+        if contact_form.is_valid():
+            contact_form.save()
+            return redirect(reverse('donaciones:donaciones')+'?ok')
+        
+        else:
+            return redirect(reverse('donaciones:donaciones')+'?error')
+    return render(response,'donaciones.html',{'form':contact_form})
