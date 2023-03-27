f33a1b40a24dca89c9c87794886ca6c5f2f87177
LinaMarcelaPena <linapena112@gmail.com>
2023-03-27 09:47:08 -0500

    Creation of new activities, deletion of activities, checklist of solved activities

--- Primavera/projects_primavera/models.py
+++ Primavera/projects_primavera/models.py
@@ -14,7 +14,9 @@ class Activities(models.Model):
     activity = models.CharField(max_length=100)
     observation = models.TextField()
     endate = models.DateField(datetime.date.today)
-    title = models.ForeignKey(Project, on_delete=models.CASCADE)  
+    title = models.ForeignKey(Project, on_delete=models.CASCADE)
+    approved  = models.BooleanField()
+    pending   = models.BooleanField()
     def __str__(self):
         return self.title
 
--- Primavera/projects_primavera/static/js/activity.js
+++ Primavera/projects_primavera/static/js/activity.js
@@ -1,11 +1,12 @@
 function completed(obj) {
-    elem=obj.parentNode.parentNode.parentNode;
+    let elem=obj.parentNode.parentNode.parentNode;
     elem.style.background=obj.checked ? '#279704' : '';
   }
 
   function pending(obj) {
-    elem=obj.parentNode.parentNode.parentNode;
+    let elem=obj.parentNode.parentNode.parentNode;
     elem.style.background=obj.checked ? '#F5A601' : '';
   }
+
 
   
