5e3c8cb071be40fa687df5222432db28bb48ebe5
LinaMarcelaPena <linapena112@gmail.com>
2023-03-30 15:35:57 -0500

    modify activities, save status from pending to completed activity

--- Primavera/projects_primavera/urls.py
+++ Primavera/projects_primavera/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import  render_projects, project_detail, activities,listactivities,delete
+from .views import  render_projects, project_detail, activities,listactivities,delete,edit,upda
 from . import views
  
 app_name = "projects_primavera"
@@ -11,6 +11,6 @@ urlpatterns = [
     path('activities',views.activities, name='activities'),
     path('listactivities',listactivities, name='listactivities'),
     path('deleteActivity/<int:id>', delete),
-
-
+    path('edit/<int:id>', edit),
+    path('upda/<int:id>', upda, name='update_data'),
     ]
